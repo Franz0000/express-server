eaf3d78529761d674f4f82bf37271f8f5837257b
francis <francis.lalaguna@gmail.com>
2024-03-01 11:02:31 +0800

    add crud routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const User = require('./models/userModel')
 
 const app = express()
+//middleware so that the app can understand json
+app.use(express.json())
 
 app.listen(3000,() =>{
     console.log(`express server app is running on port 3000`)
@@ -16,9 +19,64 @@ mongoose.connect('mongodb+srv://admin:NewPa$$w0rd@cluster0.czegh.mongodb.net/NOD
 
 
 //routes
-app.get('/', function (request, response) {
-    response.send('Hello World from Express Server')
+app.get('/users', async function (request, response) {
+  try {
+    const user = await User.find({})
+    response.status(200).json(user)
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).json({message:error.message})
+  }
 })
-app.get('/blog', function (request, response) {
-    response.send('hello blog')
+
+app.get('/user/:id', async function (request, response) {
+  try {
+    const {id} = request.params
+    const user = await User.findById(id)
+    response.status(200).json(user)
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).json({message:error.message})
+  }
+})
+
+
+app.post('/user', async(request, response) =>{
+  try {
+      const user = await User.create(request.body)
+      response.status(200).json(user)
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).json({message:error.message})
+  }
+})
+
+app.put('/user/:id', async(request, response) => {
+  
+  try {
+      const {id} = request.params
+      const user = await User.findByIdAndUpdate(id, request.body)
+      if(!user){
+        return response.status(404).json({message:"No Data found on user with id: " + id})
+      }
+      const updatedUser = await User.findById(id)
+      response.status(200).json(updatedUser)
+  } catch (error) {
+    response.status(500).jason({message:error.message})
+  }
+})
+
+app.delete('/user/:id', async(request, response) => {
+  
+  try {
+      const {id} = request.params
+      const user = await User.findByIdAndDelete(id, request.body)
+      if(!user){
+        return response.status(404).json({message:"No Data found on user with id: " + id})
+      }
+      // const updatedUser = await User.findById(id)
+      response.status(200).json({message: "date successfully Deleted!"})
+  } catch (error) {
+    response.status(500).jason({message:error.message})
+  }
 })
---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose  = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    firstName:{type:String,required:[true,"first name is required for this action."]},
+    middleName:{type:String,required:[true,"middle name is required for this action."]},
+    lastName:{type:String,required:[true,"last name is required for this action."]},
+    address:{type:String,required:[true, "address is required for this action"]},
+    gender:{type:String,required:[true, "gender is required for this action"]},
+    email:{type:String,required:[true, "email is required for this action"]},
+    userName:{type:String,required:[true, "userName is required for this action"]},
+},{timeStamps:true});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
