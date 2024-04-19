913f5c43d9d7a275d74f74dec46594654689029f
Francis lalaguna <franc@UBVPNN7599.core.unionbank.com>
2024-04-19 11:00:18 +0800

    ädd more header cors

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 let port = process.env.PORT || 8080;
