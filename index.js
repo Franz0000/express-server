bf881bdfb1630313ac2b4d6d3e9abb01f7c43a21
Francis lalaguna <franc@UBVPNN7599.core.unionbank.com>
2024-04-19 11:30:53 +0800

    update cors handling

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static('public'));
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
 
 
 let port = process.env.PORT || 8080;
